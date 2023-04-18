6e6256f9028368585d57720b9663d8f1abe6d3c9
aisha-n1 <aisha.naoman@yahoo.com>
2023-04-18 22:34:30 +0500

    first commit

---
+++ provider_k191362/android/app/src/main/kotlin/com/example/provider_k191362/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.provider_k191362
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ provider_k191362/lib/main.dart
@@ -0,0 +1,63 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_k191362/models/cart.dart';
+import 'package:provider_k191362/models/catalog.dart';
+import 'package:provider_k191362/screens/cart.dart';
+import 'package:provider_k191362/screens/catalog.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+
+GoRouter router() {
+  return GoRouter(
+    initialLocation: '/catalog',
+    routes: [
+      GoRoute(
+        path: '/catalog',
+        builder: (context, state) => const MyCatalog(),
+        routes: [
+          GoRoute(
+            path: 'cart',
+            builder: (context, state) => const MyCart(),
+          ),
+        ],
+      ),
+    ],
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return MultiProvider(
+      providers: [
+        
+        Provider(create: (context) => CatalogModel()),
+        ChangeNotifierProxyProvider<CatalogModel, CartModel>(
+          create: (context) => CartModel(),
+          update: (context, catalog, cart) {
+            if (cart == null) throw ArgumentError.notNull('cart');
+            cart.catalog = catalog;
+            return cart;
+          },
+        ),
+      ],
+      child: MaterialApp.router(
+        title: 'Provider Demo',
+        
+        routerConfig: router(),
+      ),
+    );
+  }
+}
